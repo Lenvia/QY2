53fd7881f351dae1a8e7260026e555498fd00d91
Lenvia <yrp1199@163.com>
2023-02-11 15:44:11 +0800

    update layout

--- qy-vue/src/components/OceanRecord.vue
+++ qy-vue/src/components/OceanRecord.vue
@@ -1,83 +1,87 @@
 <template>
-  <el-form class="flex_column" ref="form" :model="form" label-width="50px" style="height: 100%">
+  <el-form class="flex_column" ref="form" :model="form" label-width="80px" style="height: 100%">
     <el-row style="display:flex; background-color: #42b983">
       <el-col :span="14" class="flex_column height_adjust_equal" style="background-color: aliceblue; height: 100%">
         <el-radio-group v-model="form.resource">
-          <el-row class="ratio_content_left">
+          <el-row class="ratio-content-left">
             <el-radio label="Start Region"></el-radio>
           </el-row>
 
-          <el-row class="input_content_right">
+          <el-row class="input-content-right">
             <el-form-item class="custom-form" label="lifetime">
-              <input class="input_box" v-model="form.lifetime"/>
+              <input class="input-box" v-model="form.lifetime"/>
             </el-form-item>
           </el-row>
-          <el-row class="input_content_right">
+          <el-row class="input-content-right">
             <el-form-item class="custom-form" label="number" style="padding: 0">
-              <input class="input_box" v-model="form.number"/>
+              <input class="input-box" v-model="form.number"/>
             </el-form-item>
           </el-row>
-          <el-row class="input_content_right">
+          <el-row class="input-content-right">
             <el-form-item class="custom-form" label="depth">
-              <input class="input_box" v-model="form.depth"/>
+              <input class="input-box" v-model="form.depth"/>
             </el-form-item>
           </el-row>
 
 
-          <el-row class="ratio_content_left">
+          <el-row class="ratio-content-left">
             <el-radio label="Analysis Region"></el-radio>
           </el-row>
 
-          <el-row class="input_content_right">
+          <el-row class="input-content-right">
             <el-checkbox label="necessary" v-model="form.necessary" name="type"></el-checkbox>
           </el-row>
 
-          <el-row class="input_content_right">
+          <el-row class="input-content-right">
             <el-form-item class="custom-form" label="stay">
-              <input class="input_box" v-model="form.stay"/>
+              <input class="input-box" v-model="form.stay"/>
             </el-form-item>
           </el-row>
         </el-radio-group>
       </el-col>
-      <el-col :span="10" class="height_adjust_equal">
+      <el-col :span="10" class="height_adjust_equal flex_column" style="height: 100%">
 
-        <el-row class="content_center">
-          <div>asdad</div>
+        <el-row class="content_center" style="flex-grow: 8">
+          <div style="background-color: blueviolet; height: 90%; width: 80%">123</div>
         </el-row>
 
-        <el-row class="content_center">
-          <el-button style="height: 50%; margin-top: 15px; margin-right: 10px">Enter</el-button>
+        <el-row class="content_center" style="flex-grow: 1">
+          <el-button class="content_center" style="height: 50%; width: 80%">Record</el-button>
         </el-row>
       </el-col>
     </el-row>
+    <el-divider></el-divider>
+    <el-row style="background-color: darkkhaki">
+      <el-row class="">
+        <el-form-item class="custom-form" label="Task Name">
+          <input class="input-box" v-model="form.taskName"/>
+        </el-form-item>
+      </el-row>
 
-    <el-row style="flex-grow: 2; background-color: darkkhaki">
+      <el-row>
+        <el-form-item class="custom-form" label="Time Range">
+          <el-col :span="11" class="content_center">
+            <el-date-picker class="date-picker" type="date" icon="none" value-format="yyyy-MM-dd"
+                            v-model="form.timeRange1" style="width: 100%;"></el-date-picker>
+          </el-col>
 
+          <el-col class="line" :span="2">-</el-col>
+
+          <el-col :span="11" class="content_center">
+            <el-date-picker class="date-picker" type="date" icon="none" value-format="yyyy-MM-dd"
+                            v-model="form.timeRange2" style="width: 100%;"></el-date-picker>
+          </el-col>
+        </el-form-item>
+      </el-row>
+      <el-row class="input-content-right">
+        <el-button class="content_center" style="height: 30px; width: 40%; margin-bottom: 10px">Create Task</el-button>
+      </el-row>
     </el-row>
 
     <el-row style="flex-grow: 3">
 
     </el-row>
 
-
-    <!--    <el-form-item label="活动性质">-->
-    <!--      <el-checkbox-group v-model="form.type">-->
-    <!--        <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>-->
-    <!--      </el-checkbox-group>-->
-    <!--    </el-form-item>-->
-    <!--    <el-form-item label="活动时间">-->
-    <!--      <el-col :span="11">-->
-    <!--        <el-date-picker type="date" placeholder="开始日期" v-model="form.date1" style="width: 100%;"></el-date-picker>-->
-    <!--      </el-col>-->
-    <!--      <el-col class="line" :span="2">-</el-col>-->
-    <!--      <el-col :span="11">-->
-    <!--        <el-date-picker type="date" placeholder="截止日期" v-model="form.date2" style="width: 100%;"></el-date-picker>-->
-    <!--      </el-col>-->
-    <!--    </el-form-item>-->
-    <!--    <el-form-item>-->
-    <!--      <el-button type="primary" @click="onSubmit">立即创建</el-button>-->
-    <!--      <el-button>取消</el-button>-->
-    <!--    </el-form-item>-->
   </el-form>
 </template>
 
@@ -119,27 +123,42 @@ export default {
 }
 
 /deep/ .el-form-item__label {
-  padding: 0;
+  white-space: nowrap
 }
 
-.input_box {
+
+.input-box {
   width: 80px;
 }
 
-.input_content_right {
+.input-content-right {
   display: flex;
   justify-content: end;
   margin-right: 10px;
 }
 
-.ratio_content_left {
+.ratio-content-left {
   display: flex;
   justify-content: start;
   margin-left: 10px;
 }
 
 .custom-form {
-  margin-bottom: 0 !important;
+  margin-bottom: 0;
+}
+
+.date-picker {
+  justify-content: center;
+  align-items: center;
+  height: 10px !important;
+  font-size: 10px !important;
+}
+
+.el-divider--horizontal{
+  margin: 8px 0;
+  background: 0 0;
+  border-top: 1px solid #d2d2d2;
+
 }
 
 </style>
