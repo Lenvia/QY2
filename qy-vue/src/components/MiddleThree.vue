40db808cbb9ce8e4782b6bfc9e17466d7f210c46
Lenvia <yrp1199@163.com>
2023-02-24 18:58:25 +0800

    udpate bar chart

--- qy-vue/src/components/MiddleThree.vue
+++ qy-vue/src/components/MiddleThree.vue
@@ -36,7 +36,9 @@ export default {
 
   data() {
     return {
-
+      svg1: null,
+      svg2: null,
+      svg3: null,
       chart1: {
         labels: [],
         data1: [],
@@ -44,6 +46,8 @@ export default {
         margin: {top: 10, right: 10, bottom: 20, left: 30},
         width: 0,
         height: 0,
+        color: ["#9400D3", "#009999"],
+
       },
       chart2: {
         labels: [],
@@ -51,6 +55,7 @@ export default {
         margin: {top: 10, right: 10, bottom: 20, left: 30},
         width: 0,
         height: 0,
+        color: 'steelblue',
       },
       chart3: {
         labels: [],
@@ -58,6 +63,7 @@ export default {
         margin: {top: 10, right: 10, bottom: 20, left: 30},
         width: 0,
         height: 0,
+        color: 'steelblue',
       },
     };
   },
@@ -65,12 +71,35 @@ export default {
   mounted() {
     this.$nextTick(() => {
       // console.log(this.$refs.chartContainer1.offsetWidth)
+      // 初始化 svg
       this.chart1.width = this.$refs.chartContainer1.offsetWidth - this.chart1.margin.left - this.chart1.margin.right;
       this.chart1.height = this.$refs.chartContainer1.offsetHeight - this.chart1.margin.top - this.chart1.margin.bottom;
       this.chart2.width = this.$refs.chartContainer1.offsetWidth - this.chart2.margin.left - this.chart2.margin.right;
       this.chart2.height = this.$refs.chartContainer1.offsetHeight - this.chart2.margin.top - this.chart2.margin.bottom;
       this.chart3.width = this.$refs.chartContainer1.offsetWidth - this.chart3.margin.left - this.chart3.margin.right;
       this.chart3.height = this.$refs.chartContainer1.offsetHeight - this.chart3.margin.top - this.chart3.margin.bottom;
+
+      this.svg1 = d3.select(this.$refs.chart1)
+          .append('svg')
+          .attr('width', this.chart1.width + this.chart1.margin.left + this.chart1.margin.right)
+          .attr('height', this.chart1.height + this.chart1.margin.top + this.chart1.margin.bottom)
+          .append('g')
+          .attr('transform', `translate(${this.chart1.margin.left},${this.chart1.margin.top})`);
+
+      this.svg2 = d3.select(this.$refs.chart2)
+          .append('svg')
+          .attr('width', this.chart2.width + this.chart2.margin.left + this.chart2.margin.right)
+          .attr('height', this.chart2.height + this.chart2.margin.top + this.chart2.margin.bottom)
+          .append('g')
+          .attr('transform', `translate(${this.chart2.margin.left},${this.chart2.margin.top})`);
+
+      this.svg3 = d3.select(this.$refs.chart3)
+          .append('svg')
+          .attr('width', this.chart3.width + this.chart3.margin.left + this.chart3.margin.right)
+          .attr('height', this.chart3.height + this.chart3.margin.top + this.chart3.margin.bottom)
+          .append('g')
+          .attr('transform', `translate(${this.chart3.margin.left},${this.chart3.margin.top})`);
+
 
       // 获取数据
       axios.get('/json_statistics.json')
@@ -88,9 +117,9 @@ export default {
           .catch(error => {
             console.log(error);
           }).then(() => {  // 绘制
-        this.drawChart1();
-        this.drawChart2();
-        this.drawChart3();
+        this.drawGroupBarChart(this.svg1, this.chart1);
+        this.drawAreaChart(this.svg2, this.chart2);
+        this.drawAreaChart(this.svg3, this.chart3);
       });
 
 
@@ -98,82 +127,84 @@ export default {
   },
 
   methods: {
-
-
-    drawChart1() {
-      const svg = d3.select(this.$refs.chart1)
-          .append('svg')
-          .attr('width', this.chart1.width + this.chart1.margin.left + this.chart1.margin.right)
-          .attr('height', this.chart1.height + this.chart1.margin.top + this.chart1.margin.bottom)
-          .append('g')
-          .attr('transform', `translate(${this.chart1.margin.left},${this.chart1.margin.top})`);
-
-
+    // 分组柱状统计图
+    drawGroupBarChart(svg, chart) {
       // 计算x和y轴的比例尺
-      const xScale = d3.scaleLinear()
-          .domain([d3.min(this.chart1.labels), d3.max(this.chart1.labels)])  // 定义数据范围
-          .range([0, this.chart1.width]);  // 定义输出范围
+      const xScale = d3.scaleBand()
+          .domain(chart.data1.map((d) => d.x))
+          .range([0, chart.width]).padding(0.1);
 
       // 创建x轴对象
-      const xAxis = d3.axisBottom(xScale)
-          .tickValues(this.chart1.labels);  // 自定义刻度值
+      const xAxis = d3.axisBottom(xScale);
 
       const yScale = d3.scaleLinear()
-          .domain([0, d3.max(this.chart1.data1.concat(this.chart1.data2), d => d.y)])
-          .range([this.chart1.height, 0]);  // 由于在Web中像素坐标的原点是左上角，所以yScale输出值的范围是反向的
-
-      // 绘制面积图，将数据分别处理成面积生成器所需的格式
-      const area1 = d3.area()
-          .x(d => xScale(d.x))
-          .y0(yScale(0))
-          .y1(d => yScale(d.y))(this.chart1.data1);
-
-      const area2 = d3.area()
-          .x(d => xScale(d.x))
-          .y0(yScale(0))
-          .y1(d => yScale(d.y))(this.chart1.data2);
-
+          .domain([0, d3.max(chart.data1.concat(chart.data2), d => d.y)])
+          .range([chart.height, 0]);  // 由于在Web中像素坐标的原点是左上角，所以yScale输出值的范围是反向的
 
       // 绘制x和y轴
       svg.append('g')
-          .attr('transform', `translate(0, ${this.chart1.height})`)
+          .attr('transform', `translate(0, ${chart.height})`)
           .call(xAxis);  // 如果没有自定义x轴，那么这里应该填 d3.axisBottom(xScale)
 
       svg.append('g')
           .call(d3.axisLeft(yScale));
 
-      // 绘制面积图
-      svg.append('path')
-          .attr('d', area1)
-          .attr('fill', 'steelblue')
+
+      // 绘制柱状图。两个并列
+      const barWidth = xScale.bandwidth() / 2;
+
+      const group1 = svg
+          .selectAll(".group1")
+          .data(chart.data1)
+          .enter()  // 对于每个数据点在绑定的元素集合中创建一个矩形元素
+          .append("g")
+          .attr("class", "group1")
+          .attr("transform", (d) => `translate(${xScale(d.x)}, 0)`);
+      group1
+          .append("rect")
+          .attr("class", "bar1")
+          // .attr("x", (d) => xScale(d.x))
+          .attr("y", (d) => yScale(d.y))
+          .attr("width", barWidth)
+          .attr("height", (d) => chart.height - yScale(d.y))
+          .attr("fill", chart.color[0])
           .attr("opacity", 0.7);
 
-      svg.append('path')
-          .attr('d', area2)
-          .attr('fill', 'yellow')
+      const group2 = svg
+          .selectAll(".group2")
+
+          .data(chart.data2)
+          .enter()  // 对于每个数据点在绑定的元素集合中创建一个矩形元素
+          .append("g")
+          .attr("class", "group1")
+          .attr("transform", (d) => `translate(${xScale(d.x) + barWidth}, 0)`);
+      group2
+          .append("rect")
+          .attr("class", "bar2")
+          // .attr("x", (d) => xScale(d.x))
+          .attr("y", (d) => yScale(d.y))
+          .attr("width", barWidth)
+          .attr("height", (d) => chart.height - yScale(d.y))
+          .attr("fill", chart.color[1])
           .attr("opacity", 0.7);
+
+
+
     },
-    drawChart2(){
-      const svg = d3.select(this.$refs.chart2)
-          .append('svg')
-          .attr('width', this.chart2.width + this.chart2.margin.left + this.chart2.margin.right)
-          .attr('height', this.chart2.height + this.chart2.margin.top + this.chart2.margin.bottom)
-          .append('g')
-          .attr('transform', `translate(${this.chart2.margin.left},${this.chart2.margin.top})`);
-
-
+    // 面积图
+    drawAreaChart(svg, chart){
       // 计算x和y轴的比例尺
       const xScale = d3.scaleLinear()
-          .domain([d3.min(this.chart2.labels), d3.max(this.chart2.labels)])  // 定义数据范围
-          .range([0, this.chart2.width]);  // 定义输出范围
+          .domain([d3.min(chart.labels), d3.max(chart.labels)])  // 定义数据范围
+          .range([0, chart.width]);  // 定义输出范围
 
       // 创建x轴对象
       const xAxis = d3.axisBottom(xScale)
-          .tickValues(this.chart2.labels);  // 自定义刻度值
+          .tickValues(chart.labels);  // 自定义刻度值
 
       const yScale = d3.scaleLinear()
-          .domain([0, d3.max(this.chart2.data, d => d.y)])
-          .range([this.chart2.height, 0]);  // 由于在Web中像素坐标的原点是左上角，所以yScale输出值的范围是反向的
+          .domain([0, d3.max(chart.data, d => d.y)])
+          .range([chart.height, 0]);  // 由于在Web中像素坐标的原点是左上角，所以yScale输出值的范围是反向的
 
 
       const area = d3.area()
@@ -185,7 +216,7 @@ export default {
 
       // 绘制x和y轴
       svg.append('g')
-          .attr('transform', `translate(0, ${this.chart2.height})`)
+          .attr('transform', `translate(0, ${chart.height})`)
           .call(xAxis);  // 如果没有自定义x轴，那么这里应该填 d3.axisBottom(xScale)
 
       svg.append('g')
@@ -193,57 +224,10 @@ export default {
 
       // 绘制面积图
       svg.append('path')
-          .datum(this.chart2.data)
+          .datum(chart.data)
           .attr('d', area)
-          .attr('fill', 'steelblue')
+          .attr('fill', chart.color)
           .attr("opacity", 0.7);
-
-    },
-    drawChart3(){
-      const svg = d3.select(this.$refs.chart3)
-          .append('svg')
-          .attr('width', this.chart3.width + this.chart3.margin.left + this.chart3.margin.right)
-          .attr('height', this.chart3.height + this.chart3.margin.top + this.chart3.margin.bottom)
-          .append('g')
-          .attr('transform', `translate(${this.chart3.margin.left},${this.chart3.margin.top})`);
-
-
-      // 计算x和y轴的比例尺
-      const xScale = d3.scaleLinear()
-          .domain([d3.min(this.chart3.labels), d3.max(this.chart3.labels)])  // 定义数据范围
-          .range([0, this.chart3.width]);  // 定义输出范围
-
-      // 创建x轴对象
-      const xAxis = d3.axisBottom(xScale)
-          .tickValues(this.chart3.labels);  // 自定义刻度值
-
-      const yScale = d3.scaleLinear()
-          .domain([0, d3.max(this.chart3.data, d => d.y)])
-          .range([this.chart3.height, 0]);  // 由于在Web中像素坐标的原点是左上角，所以yScale输出值的范围是反向的
-
-
-      const area = d3.area()
-
-          .x(d => xScale(d.x))
-          .y0(yScale(0))
-          .y1(d => yScale(d.y));
-
-
-      // 绘制x和y轴
-      svg.append('g')
-          .attr('transform', `translate(0, ${this.chart3.height})`)
-          .call(xAxis);  // 如果没有自定义x轴，那么这里应该填 d3.axisBottom(xScale)
-
-      svg.append('g')
-          .call(d3.axisLeft(yScale));
-
-      // 绘制面积图
-      svg.append('path')
-          .datum(this.chart3.data)
-          .attr('d', area)
-          .attr('fill', 'steelblue')
-          .attr("opacity", 0.7);
-
     },
   }
 }
@@ -253,7 +237,7 @@ export default {
 
 .small-view {
   height: 80%;
-  width: 90%;
+  width: 95%;
   background-color: limegreen;
 }
 </style>
