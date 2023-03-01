c15ca86f26e657c43bf509dd8f8f9936ca8bbe60
Lenvia <yrp1199@163.com>
2023-03-01 17:18:41 +0800

    update path view

--- qy-vue/src/components/RightOneBP.vue
+++ qy-vue/src/components/RightOneBP.vue
@@ -16,26 +16,35 @@ export default {
       chart: null,
       chartData: {
         nodes: [
-          {id: 1, name: 'Node 1', x: 100, y: 100},
-          {id: 2, name: 'Node 2', x: 200, y: 50},
-          {id: 3, name: 'Node 3', x: 300, y: 250}
+          // {id: 1, name: 'Node 1', x: 100, y: 100},
+          // {id: 2, name: 'Node 2', x: 200, y: 50},
+          // {id: 3, name: 'Node 3', x: 300, y: 250}
         ],
         links: [
-          {source: 1, target: 2},
-          {source: 2, target: 3}
+          // {source: 1, target: 2},
+          // {source: 2, target: 3}
         ]
       },
       chartOption: {
         title: {
-          text: 'Graph on Cartesian'
+          // text: 'Graph on Cartesian'
         },
         tooltip: {},
         xAxis: {
-          type: 'value'
+          type: 'value',
+          splitLine: {
+            show: false
+          },
         },
         yAxis: {
-          type: 'value'
+          type: 'value',
+          splitLine: {
+            show: false
+          },
         },
+        // grid: {
+        //   show: false
+        // },
         series: [
           {
             type: 'graph',
@@ -60,19 +69,62 @@ export default {
 
   mounted() {
     this.$nextTick(() => {
-
+      // 初始化画布
       this.chart = echarts.init(this.$refs.scatterChart)
-      /*
-        series[0].data 需要传入一个数组，该数组中的每一项都是一个对象，该对象包含了一组数据，其中包括：
-        value：表示数据值，可以是数字或字符串；
-        name：表示数据项的名称，可以是数字或字符串；
-        itemStyle：表示数据项的样式，可以是对象或函数；
-        label：表示数据项的标签，可以是对象或函数；
-        emphasis：表示数据项的高亮状态，可以是对象或函数。
-       */
+
+      axios.get('/fake.json')
+          .then(response => {
+            const json_data = response.data;
+
+            // 解析json
+            this.chartData.nodes = json_data["nodes"];
+            this.chartData.links = json_data["links"];
+          })
+          .catch(error => {
+            console.log(error);
+          }).then(() => {  // 绘制
+
+            /*
+              series[0].data 需要传入一个数组，该数组中的每一项都是一个对象，该对象包含了一组数据，其中包括：
+              value：表示数据值，可以是数字或字符串；
+              name：表示数据项的名称，可以是数字或字符串；
+              itemStyle：表示数据项的样式，可以是对象或函数；
+              label：表示数据项的标签，可以是对象或函数；
+              emphasis：表示数据项的高亮状态，可以是对象或函数。
+             */
+            this.chartOption.series[0].data = this.chartData.nodes.map(node => ({
+              value: [node.x, node.y],
+              name: node.name,
+              symbolSize: Math.sqrt(node.size) * 10,
+              itemStyle:{
+                color: node.color,
+              }
+            }));
+
+
+            this.chartOption.series[0].links = this.chartData.links.map(link => ({
+              source: link.source,
+              target: link.target,
+              lineStyle: {
+                color: link.color,
+                width: link.width,
+                curveness: link.curveness,
+              },
+            }));
+
+            this.chart.setOption(this.chartOption)
+          });
+
+
+    });
+
+  },
+
+  watch: {
+    chartData() {
       this.chartOption.series[0].data = this.chartData.nodes.map(node => ({
-          "value": [node.x, node.y],
-          "name": node.name,
+        "value": [node.x, node.y],
+        "name": node.name,
       }));
 
 
@@ -85,9 +137,7 @@ export default {
       }));
 
       this.chart.setOption(this.chartOption)
-
-    });
-
+    },
   },
 
   methods: {}
